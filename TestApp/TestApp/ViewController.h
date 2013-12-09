cd0590bf4bce829c41831c6df19c1871784fbfab
Ben Rust <brust@cardinalsolutions.com>
2013-12-09 10:15:34 -0500

    create it!

---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Ben Rust on 12/9/13.
+//  Copyright (c) 2013 Ben Rust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestApp/TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Ben Rust on 12/9/13.
+//  Copyright (c) 2013 Ben Rust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
